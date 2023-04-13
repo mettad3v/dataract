c90c041c26d39fc0e1801304f790c485e829656a
Okonkwo Chinonso Emmanuel <milestonetech96@gmail.com>
2023-04-13 09:06:49 +0100

    ...

--- src/services/ButterCMSService.ts
+++ src/services/ButterCMSService.ts
@@ -3,7 +3,7 @@ import Butter from "buttercms";
 
 export let butterService: any = undefined;
 try {
-    const apiKey = String(import.meta.env.VITE_APP_BUTTER_CMS_API_KEY);
+    const apiKey = String("be66a8821d3ee0d8d370176b0c36d591824d35bf");
     const preview = import.meta.env.VITE_APP_BUTTER_CMS_PREVIEW !== "false";
     butterService = Butter(apiKey, preview);
 } catch (error) {
